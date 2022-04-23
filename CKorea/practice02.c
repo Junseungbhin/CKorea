c5095375a199ebecb52709cc99be781001ea560c
Junseungbhin <tmdqls104@naver.com>
2022-04-23 12:46:57 +0900

    추가

---
+++ CKorea/practice02.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main() {
+	int bhin1;
+	float bhin2;
+	char bhin3;
+	char bhin4[128] = "안녕하세요 C언어";
+
+	bhin1 = 1;
+	bhin2 = 3.14;
+	bhin3 = 'a';
+
+	printf("정수는 %d \n", bhin1);
+	printf("실수는 %f \n", bhin2);
+	printf("문자는 %c \n", bhin3);
+	printf("문자열은 %s \n", bhin4);
+
+	printf("%d, %f, %c %s\n", bhin1, bhin2, bhin3, bhin4);
+}
---
+++ CKorea/pratice01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void main4() 
+{
+	//1
+	int number1,number2;		// 두 개의  int형 변수를 한 번에 선언 
+	number1 = 1;
+	number2 = 2;
+	int number3 = 3, number4 = 4;		//변수의 선언과 초기화 동시에 진행 
+
+	//2
+	/*사용 가능한 변수명*/
+	int num = 3;	//int는 '정수'를 의미하는 자료입니다.
+	int number5 = 5;
+	int num_ber = 10;
+	int Number = 15;
+
+	/*사용 불가능한 변수명*/
+	// int hello boy = 3;				//공백 포함
+	// int hi#$ = 5;				//특수 기호 포함
+	// int 8number = 8;				//숫자로 시작
+	// int short = 3;				//기능이 있는 키워드
+
+	//3
+	int number_1 = 3;
+	printf("%d\n", number_1);
+	int number_2 = 5;
+	printf("%d\n", number_2);
+
+}

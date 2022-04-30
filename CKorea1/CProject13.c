095a20234993b0c310ba567249c57768e4fc8cd5
Junseungbhin <tmdqls104@naver.com>
2022-04-30 10:10:18 +0900

    추가

--- CKorea1/CProject13.c
+++ CKorea1/CProject13.c
@@ -1,7 +1,7 @@
 //C언어 기본포맷
 #include <stdio.h>
 
-void main() {
+void main13() {
 	// 출력
 	printf("hello world\n");
 
@@ -16,8 +16,15 @@ void main() {
 	var2 = 3.14;
 	var3 = 'a';
 
-	char var4[] = "hello world";		//변수 초기화
+	char var4[255] = "hello world";		//변수 초기화
 
 	// 변수의 출력
-	printf("%d, %f, %c, %s", var1, var2, var3, var4);
+	printf("%d, %f, %c, %s \n", var1, var2, var3, var4);
+
+	// 입력
+	scanf_s("%d", &var1);
+	printf("%d \n", var1);
+
+	scanf_s("%s", &var4, 255);		//문자열은 총 길이까지 적어주기!
+	printf("%s \n", var4);
 }
---
+++ CKorea1/CProject14.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void main() {
+	// 숫자를 2개 입력받아서 
+	// 더하기, 빼기, 곱하기, 나누기, 나머지 구하기
+
+	/*
+	[입력 예시]5, 5
+	더하기 : 10
+	빼기 : 0
+	곱하기 : 25
+	나누기 : 1
+	나머지 구하기 : 0
+	*/
+	int num1, num2;
+	printf("첫번째 숫자를 입력하세요>>");
+	scanf_s("%d",&num1);
+	printf("두번째 숫자를 입력하세요>>");
+	scanf_s("%d",&num2);
+
+	printf("더하기: %d \n", num1+num2);
+	printf("빼기: %d \n", num1 - num2);
+	printf("곱하기: %d \n", num1 * num2);
+	printf("나누기: %.1f \n", (float)num1 / num2);
+	printf("나머지구하기: %d \n", num1 % num2);
+
+	return 0;
+
+}

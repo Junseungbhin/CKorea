6e31f20a65db50a3275e40bd5504fb51e5f157bd
Junseungbhin <tmdqls104@naver.com>
2022-05-01 10:50:44 +0900

    계산기

--- Calculator/resource.h
+++ Calculator/resource.h
@@ -30,6 +30,7 @@
 #define IDC_NUM5                        1013
 #define IDC_NUM0                        1014
 #define IDC_CAL                         1015
+#define IDC_BUTTON1                     1017
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -39,7 +40,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1011
+#define _APS_NEXT_CONTROL_VALUE         1018
 #define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
